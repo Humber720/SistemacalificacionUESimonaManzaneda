3fd1c59b6678cdf13220895350380870e7bd5d76
profesorhumberto <yupconhum321@gmail.com>
2026-04-09 21:19:16 -0400

    menu.js

--- menu.js
+++ menu.js
@@ -27,8 +27,99 @@
 // BASE DE DATOS DE ESTUDIANTES
 // ===============================
 const estudiantes = {
-    "1234567": { nombre: "VALENTINA", apellido: "PEREZ QUISPE", curso: "1ro de Primaria" },
-    "7654321": { nombre: "JUAN", apellido: "LOPEZ MAMANI", curso: "2do de Primaria" }
+    // 1ro de Primaria
+    "16103032": { nombre: "ISAMAR ALEYDA", apellido: "HUANCA CANTUTA", curso: "1ro de Primaria" },
+    "17855221": { nombre: "HILDA NEYDA", apellido: "AJAHUANA CONDORI", curso: "1ro de Primaria" },
+    "16235752": { nombre: "CIELO", apellido: "QUISPE CONDORI", curso: "1ro de Primaria" },
+    "16450562": { nombre: "IGOR ALEJANDRO", apellido: "CRUZ FALCON", curso: "1ro de Primaria" },
+    "16415828": { nombre: "JASMINE", apellido: "USNAYO MAMANI", curso: "1ro de Primaria" },
+    "17663300": { nombre: "YULER EZLID", apellido: "RAMALLO MAYO", curso: "1ro de Primaria" },
+    "16225325": { nombre: "DAYANA", apellido: "CONDORI MONTAÑO", curso: "1ro de Primaria" },
+    "16882382": { nombre: "LYCEL ABRIL", apellido: "TORREZ PAUCARA", curso: "1ro de Primaria" },
+    "17612197": { nombre: "EMELETH", apellido: "ERGUETA RODRIGUEZ", curso: "1ro de Primaria" },
+    "16439814": { nombre: "MATIAS ANTONI", apellido: "QUISPE VENTURA", curso: "1ro de Primaria" },
+
+    // 2do de Primaria
+    "15505305": { nombre: "YAMILE", apellido: "LIMACHI CANTUTA", curso: "2do de Primaria" },
+    "17614196": { nombre: "HEYDAN KALEB", apellido: "MENA CHOQUE", curso: "2do de Primaria" },
+    "15325697": { nombre: "MARISOL", apellido: "CASTILLO FERNANDEZ", curso: "2do de Primaria" },
+    "15735347": { nombre: "DANIEL LUIS", apellido: "FERNANDEZ FERNANDEZ", curso: "2do de Primaria" },
+    "16372545": { nombre: "CARMEN VIRGINIA", apellido: "QUISPE HUANCA", curso: "2do de Primaria" },
+    "15538197": { nombre: "MATIAS CALEB", apellido: "MADANI HUISA", curso: "2do de Primaria" },
+    "15574544": { nombre: "YESSENIA SARAHY", apellido: "RODRIGUEZ MOLLO", curso: "2do de Primaria" },
+    "16217781": { nombre: "CAMILA", apellido: "HUANCA PEREZ", curso: "2do de Primaria" },
+
+    // 3ro de Primaria
+    "15383976": { nombre: "FRANCO GAEL", apellido: "MADANI CANTUTA", curso: "3ro de Primaria" },
+    "15098838": { nombre: "SEBASTIAN ZABDIEL", apellido: "CASTILLO CARLO", curso: "3ro de Primaria" },
+    "15200288": { nombre: "DIEGO", apellido: "TOLA CHOQUE", curso: "3ro de Primaria" },
+    "16916466": { nombre: "EDYLSON", apellido: "MARCOCHAPI CONDORI", curso: "3ro de Primaria" },
+    "16782741": { nombre: "DANITS JHANE", apellido: "QUISPE CONDORI", curso: "3ro de Primaria" },
+    "16789062": { nombre: "SHIOMARA", apellido: "AJAHUANA CONDORI", curso: "3ro de Primaria" },
+    "15099063": { nombre: "IKER ASAD", apellido: "AMARU ERGUETA", curso: "3ro de Primaria" },
+    "16787132": { nombre: "JUAN MATEO", apellido: "LUNA ESPINOZA", curso: "3ro de Primaria" },
+    "15310814": { nombre: "LEILA SCARLETT", apellido: "SEPULVEDA GUTIERREZ", curso: "3ro de Primaria" },
+    "17389914": { nombre: "NATALY", apellido: "VALLEJOS LOPES", curso: "3ro de Primaria" },
+    "17310778": { nombre: "LUZ EVELIN", apellido: "QUISPE MAMANI", curso: "3ro de Primaria" },
+    "17893355": { nombre: "CANDY", apellido: "YUPANQUI MAMANI", curso: "3ro de Primaria" },
+    "15041299": { nombre: "YARELY LUZ", apellido: "MAMANI MIRANDA", curso: "3ro de Primaria" },
+    "15171572": { nombre: "ASTRID ZOE", apellido: "ALANOCA QUISPE", curso: "3ro de Primaria" },
+    "16786439": { nombre: "BIANCA MAYTE", apellido: "MENA QUISPE", curso: "3ro de Primaria" },
+    "15275310": { nombre: "EMILY KEYLY", apellido: "QUISPE TOLA", curso: "3ro de Primaria" },
+    "15530203": { nombre: "DIEGO ALEXANDER", apellido: "QUISPE TOLA", curso: "3ro de Primaria" },
+
+    // 4to de Primaria
+    "17259685": { nombre: "THAIS DANIELA", apellido: "ARHUIRI", curso: "4to de Primaria" },
+    "16297349": { nombre: "SHARELA", apellido: "QUISPE ADUVIRI", curso: "4to de Primaria" },
+    "16653592": { nombre: "ABNER ZENON", apellido: "CASTILLO CANAZA", curso: "4to de Primaria" },
+    "16689391": { nombre: "NADIA WARA", apellido: "FLORES CASTILLO", curso: "4to de Primaria" },
+    "17414259": { nombre: "DANNA AVRIL", apellido: "CHOQUE FERNANDEZ", curso: "4to de Primaria" },
+    "17118554": { nombre: "SAMI VALERIA", apellido: "CHOQUE FLORES", curso: "4to de Primaria" },
+    "16959941": { nombre: "EUSEBIA", apellido: "QUISPE HUANCA", curso: "4to de Primaria" },
+    "15790869": { nombre: "HANS ANTHONY", apellido: "FALCON ILLANES", curso: "4to de Primaria" },
+    "17310758": { nombre: "LUZ CAMILA", apellido: "QUISPE MAMANI", curso: "4to de Primaria" },
+    "15027286": { nombre: "RUZENA ESDENKA", apellido: "RAMIREZ MAMANI", curso: "4to de Primaria" },
+    "17272587": { nombre: "DANY JAIL", apellido: "MAMANI MOLLINEDO", curso: "4to de Primaria" },
+    "15680683": { nombre: "YUAN EDUARDO", apellido: "TORREZ PAUCARA", curso: "4to de Primaria" },
+    "15960112": { nombre: "SHEYLA LUCIANA", apellido: "VILA QUISPE", curso: "4to de Primaria" },
+    "16911786": { nombre: "ISMAEL", apellido: "FLORES VENEGAS", curso: "4to de Primaria" },
+
+    // 5to de Primaria
+    "16176647": { nombre: "GUSTAVO ANTHONY", apellido: "NINA", curso: "5to de Primaria" },
+    "14427120": { nombre: "ARIANA ALESSANDRA", apellido: "CALLISAYA ZENTENO", curso: "5to de Primaria" },
+    "15605576": { nombre: "ANDERSON VLADIMIR", apellido: "LIMACHI CANTUTA", curso: "5to de Primaria" },
+    "17265383": { nombre: "SIMON SANTIAGO", apellido: "QUISPE CONDORI", curso: "5to de Primaria" },
+    "17118549": { nombre: "SAHARA SHAYA", apellido: "CHOQUE FLORES", curso: "5to de Primaria" },
+    "16160305": { nombre: "YASIT MILAN", apellido: "CHOQUE GUTIERREZ", curso: "5to de Primaria" },
+    "14480516": { nombre: "GAEL OMAR", apellido: "PEREZ LEON", curso: "5to de Primaria" },
+    "17893281": { nombre: "URIEL", apellido: "YUPANQUI MAMANI", curso: "5to de Primaria" },
+    "15141072": { nombre: "DAMIAN NOEL", apellido: "CONDORI MAYTA", curso: "5to de Primaria" },
+    "16324027": { nombre: "LILIANA ITCEL", apellido: "ALI MAYTA", curso: "5to de Primaria" },
+    "17173764": { nombre: "BEYMAR ANGEL", apellido: "LUNA PEÑALOZA", curso: "5to de Primaria" },
+    "16217837": { nombre: "DANIELA", apellido: "HUANCA PEREZ", curso: "5to de Primaria" },
+    "14802040": { nombre: "CORALAIN AYDEE", apellido: "FUNES PEREZ", curso: "5to de Primaria" },
+    "16700337": { nombre: "JACIEL", apellido: "ALANOCA QUISPE", curso: "5to de Primaria" },
+    "15204952": { nombre: "NEYMAR", apellido: "MAMANI QUISPE", curso: "5to de Primaria" },
+ 
+    // 6to de Primaria
+    "16297336": { nombre: "SHARAI", apellido: "QUISPE ADUVIRI", curso: "6to de Primaria" },
+    "14479026": { nombre: "ALISON MAYA", apellido: "FLORES ALVAREZ", curso: "6to de Primaria" },
+    "15645677": { nombre: "YADIEL", apellido: "HUANCA CANTUTA", curso: "6to de Primaria" },
+    "16789053": { nombre: "JHOSTIN", apellido: "AJAHUANA CONDORI", curso: "6to de Primaria" },
+    "17451940": { nombre: "MAIRYN ZULAMI", apellido: "AMARU ERGUETA", curso: "6to de Primaria" },
+    "17414278": { nombre: "ANYHELO MATEO", apellido: "CHOQUE FERNANDEZ", curso: "6to de Primaria" },
+    "16789847": { nombre: "MARY LUZ", apellido: "FERNANDEZ FERNANDEZ", curso: "6to de Primaria" },
+    "16883745": { nombre: "MAXIMILIANO", apellido: "QUISPE HUANCA", curso: "6to de Primaria" },
+    "14047775": { nombre: "ALEJANDRO", apellido: "USNAYO MAMANI", curso: "6to de Primaria" },
+    "14304209": { nombre: "ISABELLA ANABEL", apellido: "SILVA ORELLANA", curso: "6to de Primaria" },
+    "14801125": { nombre: "ANDREI GEMIO", apellido: "CASTILLO PARICAHUA", curso: "6to de Primaria" },
+    "15333051": { nombre: "HAROL JHONNATAN", apellido: "ERGUETA PAUCARA", curso: "6to de Primaria" },
+    "15960084": { nombre: "SERGIO ANDREW", apellido: "VILA QUISPE", curso: "6to de Primaria" },
+    "16220469": { nombre: "NOEMI YESICA", apellido: "FERNANDEZ QUISPE", curso: "6to de Primaria" },
+    "16166926": { nombre: "THIAGO ZHAIR", apellido: "QUISPE QUISPE", curso: "6to de Primaria" },
+    "17184594": { nombre: "CAMILA SHELLY", apellido: "RIOS TERRAZAS", curso: "6to de Primaria" },
+    "16960822": { nombre: "MAGALI", apellido: "FLORES VENEGAS", curso: "6to de Primaria" },
+
 };
 
 // ===============================
